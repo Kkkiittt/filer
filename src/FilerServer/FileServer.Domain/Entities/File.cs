5141ce88c6a1ed0d7dc9243bf5ca388050c3747b
Kkkiittt <khamidov357@gmail.com>
2023-01-20 12:46:44 +0500

    created models, migration, appdbcontext

---
+++ src/FilerServer/FileServer.Domain/Entities/File.cs
@@ -0,0 +1,8 @@
+﻿namespace FileServer.Domain.Entities;
+
+public class File : BaseEntity
+{
+	public int UserId { get; set; }
+	public virtual User User { get; set; }
+	public string Name { get; set; } = string.Empty;
+}
---
+++ src/FilerServer/FileServer.Domain/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿namespace FileServer.Domain.Entities;
+
+public class User : BaseEntity
+{
+	public string Name { get; set; } = string.Empty;
+	public string Email { get; set; } = string.Empty;
+	public string Password { get; set; } = string.Empty;
+	public string Salt { get; set; } = string.Empty;
+	public long MegaBytes { get; set; }
+	public virtual IEnumerable<File> Files { get; set; } = new List<File>();
+}
