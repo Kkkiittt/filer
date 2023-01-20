ff3d75adb3cc0b07f167ca1823a7893a79d1f6c2
Kkkiittt <khamidov357@gmail.com>
2023-01-20 18:40:14 +0500

    с чистого листа

--- src/FilerServer/FileServer.Domain/Entities/File.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FileServer.Domain.Entities;
-
-public class File : BaseEntity
-{
-	public long UserId { get; set; }
-	public virtual User User { get; set; }
-	public string Name { get; set; } = string.Empty;
-	public string Hash{ get; set; } = string.Empty;
-}
